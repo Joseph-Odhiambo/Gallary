d45829cf977dc59aaefb950c613cb667daf945e5
Joseph-Odhiambo <josephkdo@gmail.com>
2020-10-10 08:37:43 +0300

    push to heroku

--- pics/tests.py
+++ pics/tests.py
@@ -17,24 +17,27 @@ class ImageTestClass(TestCase):
         Category.objects.all().delete()
         Image.objects.all().delete()
 
+        # Testing Instances
     def test_instance(self):
         self.assertTrue(isinstance(self.img1,Image))
 
-             # Testing Save Method
+        # Testing Save Method
     def test_save_method(self):
         images = Image.objects.all()
         self.assertTrue(len(images) > 0)
          
-         #Test
+        # Testing Get-Pics Method
     def test_get_pics_today(self):
         today_pics = Image.todays_pics()
         self.assertTrue(len(today_pics) > 0)
 
+        # Testing Delete Method
     def test_delete_method(self):
         self.img1.delete_image()
         images = Image.objects.all()
         self.assertTrue(len(images) == 0)
 
+        # Testing Update Method
     def test_update_method(self):
         updated_rows = Image.update_image(self.img1.id, image_name = 'The sun')
         self.assertTrue(updated_rows == 1)
@@ -52,13 +55,13 @@ class ImageTestClass(TestCase):
 
 
 
-#          # Testing Save Method
-#     def test_save_method(self):
-#         self.img1.save_image()
-#         images = Image.objects.all()
-#         self.assertTrue(len(images) > 0)
+    ## Testing Save Method
+    #  def test_save_method(self):
+    #     self.img1.save_image()    
+    #     images = Image.objects.all()
+    #     self.assertTrue(len(images) > 0)
 
-# first create a test for Areas and then create a test for category and then use the self.area and self.category to test for image,,,
+    # first create a test for Areas and then create a test for category and then use the self.area and self.category to test for image,,,
     #  def test_save_method(self):
     #     self.james.save_editor()
     #     editors = Editor.objects.all()
